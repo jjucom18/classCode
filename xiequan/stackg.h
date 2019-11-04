65a8c6456aa8bc58f9a2c5062241328f17ce0b92
xiequan277 <1185525531@qq.com>
2019-11-04 22:54:27 +0800

    commit xiequan

---
+++ xiequan/stackg.h
@@ -0,0 +1,23 @@
+#ifndef stackg_h
+#define stackg_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MaxSize 10
+typedef int DataType;
+typedef struct{
+	DataType data[MaxSize];
+	int top;
+}SeqStack;
+
+SeqStack *initStack();
+bool emptyStack(SeqStack *stack);
+bool fullStack(SeqStack *stack);
+void push(SeqStack *stack, DataType data);
+void pop(SeqStack *stack, DataType *data);
+void topStack(SeqStack *stack, DataType *data);
+
+
+
+#endif
