009c1fc84411518ddd4b7d3ec76dbdc9cf272c0e
root <root@LAPTOP-ULOUM7QU.localdomain>
2019-11-04 15:19:58 +0800

    shutao

---
+++ shutao/work.c
@@ -0,0 +1,23 @@
+nverse(LinkList &L){
+
+	   LNode *p,*q;
+	      p=L->next L
+		         H->next=NULL;
+
+	         while(p!=NULL){
+
+			    q=p;  p=q->next;
+			       q->next=L->next;
+			          L->next=q;
+
+
+				     }
+
+
+
+
+
+
+
+		     }
+~    
