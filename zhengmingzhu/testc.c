f5a7a52aa5e65624f56650bf265fd6a4d3c4673b
xiongzhili <3337826949@qq.com>
2019-11-04 14:35:25 +0800

    commit zhengmingzhu

---
+++ zhengmingzhu/testc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(int argc, char **argv)	
+     
+	char *name;
+	int id;
+	double *record;
+  Student;
+{ 
+     printf("please input name");
+	 printf("plesse input id");
+	 printf("please input record");
+
+	 return;
+}
