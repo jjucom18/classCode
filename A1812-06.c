01edc4ab4af539ff88bf894834557b3056dba0e6
easdter <2373528598@qq.com>
2019-11-04 12:02:00 +0800

    commit scvs

---
+++ A1812-06.c
@@ -0,0 +1,44 @@
+#include "stdio.h"
+#include "stdlib.h"
+typedef struct LNode
+   { int  data;
+	   struct LNode *next;
+   }  LNode, *LinkList;
+      LinkList L;
+                                                                                            
+LinkList Creat_LinkList() 
+	  { LNode *s, *r=NULL;                                                                        		
+		  int x;
+		  scanf("%d",&x);
+		  while (x!=100)  {
+			  s=(LNode *)  malloc(sizeof(LNode)  )  ;
+			  s->data=x;
+			  if(L == NULL)   L=s;      
+			  else r->next=s;
+			  r=s;
+			  scanf("%d",&x)  ;                                                                   
+		  }                                                                 
+		  if(r!=NULL)    r->next=NULL;                                      
+		  return L;                                                             
+	  }                                                                                             
+LinkList reverse()   {
+	LNode *p,*q;
+	p=L->next;
+	L->next=NULL;
+	while (p)   {
+		q=p ;   p=p->next;                      
+		q->next=L->next;                    
+		L->next=q;                                                                                      
+	}                                                                   
+}                                                                                               
+LinkList dispList()                                                                                 
+{LinkList  p=L;                                                                         
+	while(p!=NULL)   {
+		printf("%d",p->data) ;                               
+		printf(" ")  ;                                  
+		p=p->next;  }       
+	 }                                                                                      
+int  main ()  {
+	Creat_LinkList()  ;                                                       
+	reverse()  ;                                                      
+	dispList() ;}
