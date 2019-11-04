e2227fd4f93ccc87c644906d59017fb9205b5f73
root <root@LAPTOP-6N7QSHEE.localdomain>
2019-11-04 19:27:40 +0800

    A1812   3

--- hujunwei/testc.c
+++ hujunwei/testc.c
@@ -4,58 +4,37 @@ Classmate initStu()
 {
 	Classmate *ptrStu = (Classmate *)malloc(sizeof(Classmate));
 
-			printf("please input name: ");
-
-				scanf("%s", ptrStu->name);
-
-					printf("please input id: ");
-
-						scanf("%d", ptrStu->id);
-
-							printf("please input record: ");
-
-								scanf("%f", ptrStu->record);
-
-
-
-									return *ptrStu;
+	printf("please input name: ");
+	scanf("%s", ptrStu->name);
+	printf("please input id: ");
+	scanf("%d", ptrStu->id);
+	printf("please input record: ");
+	scanf("%f", ptrStu->record;)
+			
+		return *ptrStu;
 
 }
 
+void outStu(Classmate stu)
+{
+	printf("%s's id: %d and record: %f\n", stu.name, stu.id, stu.record);
 
-
-void outStu(Classmate stu){
-
-		printf("%s's id: %d and record: %f\n", stu.name, stu.id, stu.record);
-
-
-
-			return;
+	return;
 
 }
-
-
 
 float getRec(Classmate stu){
 
-		return stu.record;
+	return stu.record;
 
 }
 
 
 
 void setRec(int id, float record){
-
-		Classmate stu;
-
-			if(stu.id == id){
-
-						stu.record = record;
-
-							}
-
-
-
-				return;
-
+	Classmate stu;
+	if(stu.id == id){
+		stu.record = record;
+	}
+	return;
 }
