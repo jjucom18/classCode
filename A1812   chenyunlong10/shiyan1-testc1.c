8f9dc36a1f792fd238ffb154b63c6759eec5591b
chenyunlong <57089798+CYL-10@users.noreply.github.com>
2019-11-12 22:26:56 +0800

    Update shiyan1-testc1.c

--- A1812   chenyunlong10/shiyan1-testc1.c
+++ A1812   chenyunlong10/shiyan1-testc1.c
@@ -12,14 +12,14 @@
      int var2;
      int var3;
      printf("请输入var1,var2,var3\n");
-     scanf("%d,%d,%d\n",&var1,&var2,&var3);
+     scanf("%d,%d,%d",&var1,&var2,&var3);
+     printf("%d,%d,%d\n",var1,var2,var3);
      int *ptr_var1;
      int *ptr_var2;
      int *ptr_var3;
      ptr_var1=&var1;
      ptr_var2=&var2;
      ptr_var3=&var3;
-     printf("%d,%d,%d\n",var1,var2,var3);
      printf("%p,%p,%p\n",&var1,&var2,&var3);
      swap(ptr_var1,ptr_var2,ptr_var3);
      printf("%d,%d,%d\n",var1,var2,var3);
