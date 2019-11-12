73af36b6dbfd03329088a4a4f1cab4cb5290710e
chenyunlong <57089798+CYL-10@users.noreply.github.com>
2019-11-12 20:38:48 +0800

    Update shiyan1-testc2.c

--- 澄云龙/shiyan1-testc2.c
+++ 澄云龙/shiyan1-testc2.c
@@ -1,12 +1,17 @@
- swap(int *p1,int *p2)
+#include<stdio.h>
+int main (int argv,char **argc)
 {
-    int *p;
-       p=p1;p1=p2;p2=p
-   }
-   main() {
-       int a,b;int *pt1,*pt2;
-       scanf("%d%d",&a,&b);
-      pt1=&a;pt2=&b;
-      if(a<b) swap(pt1,pt2);
-      printf("%d%d\n",*pt1,*pt2);  
-      }
+	int a,b;
+	int *pt1,*pt2;
+	printf("请输入a,b");
+	scanf("%d,%d",&a,&b);
+	pt1=&a;pt2=&b;
+	if(a<b)swap(pt1,pt2);	
+	printf("%d,%d\n",*pt1,*pt2); 
+}
+void  swap(int*p1,int*p2)
+	{int p;
+		p=*p1;
+		*p1=*p2;
+		*p2=p;
+	}
