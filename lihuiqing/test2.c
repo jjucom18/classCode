94bfd2fce8e8255f8afecdc9ebaf1ff9f84fb3d3
lihuiqing00 <li1187721910@163.com>
2019-11-12 21:27:59 +0800

    commit test

---
+++ lihuiqing/test2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+swap(int *p1,int *p2)
+{int *p;
+	p=p1;p1=p2;p2=p
+}
+main()
+{int a,b;int *p1,*p2;
+	scanf("%d,%d",&a,&b);
+	pt1=&a;pt2=&b;
+	if(a<b) swap(pt1,pt2);
+	printf("%d,%d\n",*pt1,*pt2);
+}
