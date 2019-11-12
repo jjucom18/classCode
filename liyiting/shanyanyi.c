a510633f6ad03f91bb9d3ed64fe997c904c72b16
liyiting <leepeggy-@outlook.com>
2019-11-12 22:22:31 +0800

    commit shangyanyi

--- liyiting/shanyanyi.c
+++ liyiting/shanyanyi.c
@@ -4,29 +4,19 @@
 int main()
 {
 	int var1,var2,var3;
- 	scanf("%d,%d,%d",&var1,&var2,&var3)；
-        printf("var1=%d\n,var2=%d\n,var3=%d\n",var1,var2,var3);
-	       printf("\n");
+        scanf("%d,%d,%d",&var1,&var2,&var3);
+        printf("var1=%d,var2=%d,var3=%d",var1,var2,var3);
+        printf("\n");
+	printf("&var1=%p\n,&var2=%p\n,&var3=%p\n",&var1,&var2,&var3);
+        printf("\n");
+        int *ptr_var1, *ptr_var2, *ptr_var3;
+	int p;
+	p = *ptr_var1;
+	*ptr_var1 = *ptr_var3;
+	*ptr_var3 = *ptr_var2;
+	*ptr_var2 = p;
         
-	       
-	int *prt_var1,*prt_ar2,*prt_var3;
-    
-	printf("var1=%x\n,var2=%x\n,var3=%x\n",&var1,&var2,&var3);
-		printf("\n"); 
-       
-	int *p = NULL,*s = NULL;*q = NULL;
-	*p = *var1;
-	*s = *var2;
-	*m = *var3;
-	*var1 = *var3;
-        *var2 = *var1;
-        *var3 = *var2;
-	printf("var1=%d\n,var2=%d\n,var3=%d\n",var1,var2,var3);
-	       prinntf("\n");
-	
-
-
-
+	printf("var1=%d,var2=%d,var3=%d",var1,var2,var3);
 
 	return 0;
 }
