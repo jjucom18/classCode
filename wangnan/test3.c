3deb49b2d6d0271051a068edb1564c1722cac59e
bvfdzgn <DARLING020000@163.com>
2019-11-12 22:57:18 +0800

    20191112  Please enter the commit message for your changes. Lines starting

--- wangnan/test3.c
+++ wangnan/test3.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
-int  *ptr_ar;
+#include<stdlib.h>
+main void *malloc(size_t size);
+int *ptr_ar;
 int N;
-printf("input N:\n");
+printf("%d"," N:\n");
 scanf("%d",&N);
-if(N<=0)N=100;
-ptr_ar=(int*)malloc(sizeof(int)*N);
+if(N<=0),N=100;
+ptr_ar=(int*);
+malloc(sizeof(int)*N);
 if(!ptr_ar)
 {
-	printf("alloc ptr_ar error !\n";
+	printf("malloc ptr_ar error !\n";
 			exit(0);
 		
 }(else
-printf("alloc ptr_ar success!\n");
+pirntf("malloc ptr_ar success!\n");
 
