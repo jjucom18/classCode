bc5b05a5bf16edc84396690615118c6306567a35
root <root@SC-201911050900.localdomain>
2019-11-14 20:50:14 +0800

    2018-26

--- zhanghongle/20191114test1.c
+++ zhanghongle/20191114test1.c
@@ -1,98 +1,85 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define Error 0
-#define OK 1
-    typedef int ElemType;
-    typedef struct node{
-    ElemType element;
-    struct node *link;
-}node;
-    typedef struct{
-    struct node *head;
-    int n;
-}headerList;//头结点的插入
-    typedef int Status;
-    Status Init(headerList *h){
-    h->head=(node*)malloc(sizeof(node));
-    if(!h->head)
-    return Error;
-    h->head->link=NULL;
-    h->n=0;
-    return OK;
-}
-    Status Insert(headerList *h,int i,ElemType *x){
-    node *p,*q;
-    int j;
-    if(i<-1||i>h->n-1)
-    return Error;
-     p=h->head;
-    for(j=0;j<=i;j++) p=p->link;
-            q=malloc(sizeof(node));
-    q->element=x;
-    q->link=p->link;
-    p->link=q;
-    h->n++;
-    return OK;
-}
-    Status Output(headerList *h,int i,ElemType *x){
-    int i=0;
-    node *p;
-    if(!h->head)
-    return Error;
-    p=h->head;
-    for(i=0;i<h->n;i++){
-    p=p->link;
-    printf("%d",p->element);
-    }
-    return OK;
-}
-    Status Delete(headerList *h,int i){
-    int j;
-    node *p,*q;
-    if(!h->n)
-	 return Error;
-    if(i<0||i>h->n-1)
-	 return Error;
-    q=h->head;
-    for(j=0;j<i;j++)q=q->link;
-    p=q->link;
-    q->link=p->link;
-    free(p);
-    h->n--;
-    return OK;   
-}
-    Status Find(headerList h,int i,ElemType *x){
-    node *p;
-    int j;
-    if(i<0||i>h->n-1)
-	return Error;
-    p=h.head->link;
-    for(j=0;j<i;j++) p=p->link;
-    *x=p->element;
-    return OK;   
-}
-    void Destroy(headerList *h){
-    node *p;
-    while(h->head){
-    p=h->head->link;
-    free(h->head);
-    h->head=p;
-    }    
-}
-    void main(){
-    int i;
-    int x;
-    headerList list;
-    Init(&list);
-    for(i=0;i<9;i++)
-	    Insert(&list,i-1,i);
-    printf("插入数据是:");
-    Output(&list);
-    Delete(&list,0);
-    printf("\n插入数据是:");;
-    Find(list,0,&x);
-    printf("\n查找结果是:");
-    printf("%d",1);
-    Destroy(&list);
-}    
+#include <stdio.h>
+#include <malloc.h>
+typedef struct node
+{
+    int data;
+    struct node *next;
+}Linklist;
 
+Linklist *creat(){
+    Linklist *phead;
+    phead=(Linklist *)malloc(sizeof(Linklist));
+    phead->next=NULL;
+    return phead;
+}//头插
+void head_insert(Linklist *phead,int n){
+    Linklist *p;
+    int i;
+
+    for(i=0;i<n;i++){
+        p=(Linklist *)malloc(sizeof(Linklist));
+        scanf("%d",&p->data);
+
+        p->next=phead->next;
+        phead->next=p;
+    }
+}//尾插
+void tail_insert(Linklist *phead,int value){
+    Linklist *p,*t;
+    int i;
+    t=phead;
+
+    p=(Linklist *)malloc(sizeof(Linklist));
+    p->data=value;
+    while(t->next!=NULL)
+        t=t->next;
+
+    t->next=p;
+    p->next=NULL;
+}//打印链表
+
+void print(Linklist *phead){
+    Linklist *p;
+    p=phead->next;
+    if(p==NULL)
+        printf("此链表为空.\n");
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+void delete(Linklist *phead,int value)
+{
+    Linklist *p,*t;
+    t=phead;
+    while(t->next!=NULL)
+    {
+        if(t->next->data==value)
+        {
+            p=t->next;
+            t->next=p->next;
+            free(p);
+        }
+        else
+            t=t->next;
+    }
+}//删除
+int main(void)
+{
+    Linklist *phead;
+    int value,num;
+    phead=creat();
+    head_insert(phead,5);
+    print(phead);
+    printf("输入插入的数字:\n");
+    scanf("%d",&value);
+    tail_insert(phead,value);
+    print(phead);
+
+    printf("输入删除的数字:\n");
+    scanf("%d",&num);
+    delete(phead,num);
+    print(phead);
+}//测试
