bd1af2e0f4cd0c1aec700d0b320fb9dc092a3107
peggyee <leepeggy-@outlook.com>
2019-11-21 02:03:07 +0800

    commit link

---
+++ liyiting/practic3/test.c
@@ -0,0 +1,17 @@
+#include "link.h"
+#include "rankArr.h."
+
+int main(int argc,char **argv){
+
+	int arr[10];
+	outArr(arr,10);
+	void insertSort(arr,10);
+	outArr(arr,10);
+	void bubbleSort(arr,10);
+        outArr(arr,10);
+	void selectSort(arr,10);
+        outArr(arr,10);
+	void shellSort(arr,10);
+        outArr(arr,10);
+}
+
