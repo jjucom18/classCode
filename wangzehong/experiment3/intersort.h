fe8306d488fa105f0b3c7cee1138c574494ce91a
wzhdytz <57128069+wzhdytz@users.noreply.github.com>
2019-11-21 18:32:58 +0800

    Create intersort.h

---
+++ wangzehong/experiment3/intersort.h
@@ -0,0 +1,20 @@
+#ifndef interSort_h
+#define interSort_h
+#define Maxsize (20)
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int data;
+void Exchangesort(data[],int n);
+void Bubblesort(data[],int n);
+
+int Partition(data[],int low,int high);
+void Quickinsort(data[],int n);
+void Easychsort(data[],int n);
+
+
+
+#endif
+~                                                                                                                       ~         
