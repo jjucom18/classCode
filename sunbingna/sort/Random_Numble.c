f37ab740fc29a4a461204bbb7b2532e425c71919
sunbingnadd <s12345bn@163.com>
2019-11-21 15:26:05 +0800

    add

--- sunbingna/sort/Random_Numble.c
+++ sunbingna/sort/Random_Numble.c
@@ -3,13 +3,12 @@
 int *Random_Numble()
 {
 	srand((int)time(NULL));
-	DataType r[MaxSize];
 	for (int i=0; i < MaxSize; i++)
 	{
 		r[i] = random_1(1,20);
 	}
-	for(int i=0; i < MaxSize; i++)
-	{printf("%d\t",r[i]);}
-	printf("\n");
-	return r;
+	//for(int i=0; i < MaxSize; i++)
+	//{printf("%d\t",r[i]);}
+	//printf("\n");
+	return &r[0];
 }
