dfa5c785c933fbc90b11882ff4f7032950b9c06c
LiuJiPeng20 <57102015+LiuJiPeng20@users.noreply.github.com>
2019-11-21 12:50:28 +0800

    Update insertSort.c

--- liujipeng11-43/ShiYan3.c/insertSort.c
+++ liujipeng11-43/ShiYan3.c/insertSort.c
@@ -1,21 +1,46 @@
 #include<stdio.h>
 #include<stdio.h>
 
-void insertSort(int line[], int lineNum, int *compareTime, int *moveTime)
+void insertSort(int arr[], int n)
 {
-	for (int i = 1; i < lineNum; i++)
-	{
-		int temp = line[i];
-		int j = 0;
-		for (j = i - 1; j >= 0 && temp < line[j]; j--)
-		{
-			line[j + 1] = line[j];
-			*compareTime += 1;
-			*moveTime += 1;
-		}
-		j++;
-		line[j] = temp;
-		*moveTime += 1;
-	}
+    int i, j, tmp;
 
+    for(i = 1; i < n; i++)
+    {
+        for(j = i; j > 0; j--)
+        {
+            if(arr[j] < arr[j-1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = tmp;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    return;
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return;
+}
+
+void main()
+{
+    int arr[10] = {2,5,6,4,3,7,9,8,1,0};
+    printArray(arr, 10);
+    insertSort(arr, 10);
+    printArray(arr, 10);
+    return;
 }
