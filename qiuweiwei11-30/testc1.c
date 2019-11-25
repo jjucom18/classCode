876e67c6587880859b426ee2a9e11dbded4a76d4
qiuweiwei-qing <57095281+qiuweiwei-qing@users.noreply.github.com>
2019-11-25 14:25:17 +0800

    Update testc1.c

--- qiuweiwei11-30/testc1.c
+++ qiuweiwei11-30/testc1.c
@@ -8,17 +8,21 @@ void swap(int*p1,int*p2,int*p3)
 	          *p2=*p3;
 		      *p3=temp;}
 
-		      int main()
+int main()
 {
 	int var1,var2,var3;
 	int *ptr_var1,*ptr_var2,*ptr_var3;
+	
 	scanf("%d%d%d",&var1,&var2,&var3);
 	ptr_var1=&var1;
 	ptr_var2=&var2;
 	ptr_var3=&var3;
-	 printf("var1=%d,var2=%d,var3=%d\n",var1,var2,var3);
-	  printf("var1=%p,var2=%p,var3=%p\n",&var1,&var2,&var3);
-	    swap(ptr_var1,ptr_var2,ptr_var3);                                                                                           
-      printf("var1=%d,var2=%d,var3=%d\n",var1,var2,var3);
+	printf("change before\n");
+	printf("var1=%d,var2=%d,var3=%d\n",var1,var2,var3);
+	printf("var1=%p,var2=%p,var3=%p\n",&var1,&var2,&var3);
+	swap(ptr_var1,ptr_var2,ptr_var3);
+	printf("change after\n");
+        printf("var1=%d,var2=%d,var3=%d\n",var1,var2,var3);
+	printf("var1=%p,var2=%p,var3=%p\n",&var1,&var2,&var3);
 	     return 0;
 }
