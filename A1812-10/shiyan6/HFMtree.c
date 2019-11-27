58bde789e9fc824d706f3a4dde4c0a0141c2c97e
CYL-10 <2411493800@qq.com>
2019-11-27 23:04:53 +0800

    commit

--- A1812-10/shiyan6/HFMtree.c
+++ A1812-10/shiyan6/HFMtree.c
@@ -1,88 +1,94 @@
-#include<iostream>
-#include<iomanip>
-using namespace std;
-// 哈夫曼树的结点结构
-struct element
+#include"HFMtree.h"
+
+void Select(HuffmanTree ht,int j,int *s1,int *s2)
 {
-    int weight;        // 权值域
-    int lchild, rchild, parent;  // 该结点的左、右、双亲结点在数组中的下标
-};
-// 选取权值最小的两个结点
-void selectMin(element a[],int n, int &s1, int &s2)
-{
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i].parent == -1)// 初始化s1,s1的双亲为-1
-        {
-            s1 = i;
-            break;
+    int i; 
+    //int占4个字节，最大为2147483647
+    int min = 2147483647;
+    for(i=1;i<=j;i++){
+        if((ht[i].parent == 0) && ht[i].weight < min){
+            min = ht[i].weight;
+            *s1 = i;
+        }
+
+    }
+    int lessmin = 2147483647;
+    for(i=1;i<=j;i++){
+        if((ht[i].parent == 0) && ht[i].weight < lessmin && i != *s1){
+        //下标不能相同
+            lessmin = ht[i].weight;
+            *s2 = i;
         }
     }
-    for (int i = 0; i < n; i++)// s1为权值最小的下标
-    {
-        if (a[i].parent == -1 && a[s1].weight > a[i].weight)
-            s1 = i;
+}
+
+
+//建立哈夫曼树
+void CrtHuffmanTree(HuffmanTree ht, int w[], int n)
+{
+    int m,i;
+    m = 2*n-1;
+    for(i=1;i<=n;i++){ 
+        ht[i].weight = w[i];//初始化前n个元素成为根结点
+        ht[i].parent = 0;
+        ht[i].Lchild = 0;
+        ht[i].Rchild = 0;
     }
-    for (int j = 0; j < n; j++)
+    for(i=n+1;i<=m;i++){    //初始化后n-1个元素
+        ht[i].weight = 0;
+        ht[i].parent = 0;
+        ht[i].Lchild = 0;
+        ht[i].Rchild = 0;
+    }
+    for(i=n+1;i<=m;i++)    //从第n+1个元素开始构造新结点
     {
-        if (a[j].parent == -1&&j!=s1)// 初始化s2,s2的双亲为-1
-        {
-            s2 = j;
-            break;
+        int s1,s2;
+        //在ht的前i-1项中选择双亲为0且全值较小的两结点s1,s2
+        Select(ht,i-1,&s1,&s2);
+        ht[i].weight = ht[s1].weight + ht[s2].weight;//建立新结点，赋权值
+        ht[i].Lchild = s1;
+        ht[i].Rchild = s2;  //赋新结点左右孩子的指针
+        ht[s1].parent = i;
+        ht[s2].parent = i;  //改s1,s2的双亲指针
+    }
+}
+
+//哈夫曼编码
+void CrtHuffmanCode1(HuffmanTree ht,huffmanCode hc,int n)
+{
+    //从叶子到根，逆向求各叶子结点的编码
+    char *cd;
+    int start,i,c,p;
+    cd = (char * )malloc(n*sizeof(char ));//临时编码数组
+    cd[n-1] = '\0';          //从后向前逐位求编码，首先放置结束符
+    for(i=1;i<=n;i++)        //从每个叶子开始，求相应的哈夫曼编码
+    {
+        start = n-1;
+        c = i;
+        p = ht[i].parent;    //c为当前节点，p为其双亲
+        while(p!=0){
+            --start;
+            if(ht[p].Lchild == c)
+                cd[start] = '0';//左分支为'0'
+            else
+                cd[start] = '1';//右分支为'1'
+            c = p;
+            p = ht[p].parent;   //上溯一层 
+        }
+        hc[i] = (char *)malloc((n-start)*sizeof(char)); //动态申请编码空间
+        strcpy(hc[i],&cd[start]);                       //复制编码
+    }
+}
+
+//在生成的哈夫曼编码中查询目标
+int find_code(huffmanCode hc,int n,char *dest,int *result)
+{
+    int i ;
+    for(i=1;i<=n;i++){
+        if(strcmp(dest,hc[i])==0){
+            *result = i;
+            return 1;
         }
     }
-    for (int j = 0; j < n; j++)// s2为另一个权值最小的结点
-    {
-        if (a[j].parent == -1 && a[s2].weight > a[j].weight&&j != s1)
-            s2 = j;
-    }
-}
-// 哈夫曼算法
-// n个叶子结点的权值保存在数组w中
-void HuffmanTree(element huftree[], int w[], int n)
-{
-    for (int i = 0; i < 2*n-1; i++)    // 初始化，所有结点均没有双亲和孩子
-    {
-        huftree[i].parent = -1;
-        huftree[i].lchild = -1;
-        huftree[i].rchild = -1;
-    }
-    for (int i = 0; i < n; i++)    // 构造只有根节点的n棵二叉树
-    {
-        huftree[i].weight = w[i];
-    }
-    for (int k = n; k < 2 * n - 1; k++) // n-1次合并
-    {
-        int i1, i2; 
-        selectMin(huftree, k, i1, i2); // 查找权值最小的俩个根节点，下标为i1,i2
-        // 将i1，i2合并，且i1和i2的双亲为k
-        huftree[i1].parent = k;
-        huftree[i2].parent = k;
-        huftree[k].lchild = i1;
-        huftree[k].rchild = i2;
-        huftree[k].weight = huftree[i1].weight + huftree[i2].weight;
-    }
-    
-}
-void print(element hT[],int n)
-{
-    cout << "index weight parent lChild rChild" << endl;
-    cout << left;    // 左对齐输出 
-    for (int i = 0; i < n; ++i) 
-    {
-        cout << setw(5) << i << " ";
-        cout << setw(6) << hT[i].weight << " ";
-        cout << setw(6) << hT[i].parent << " ";
-        cout << setw(6) << hT[i].lchild << " ";
-        cout << setw(6) << hT[i].rchild << endl;
-    }
-}
-int main()
-{
-    int x[] = { 5,29,7,8,14,23,3,11 };        // 权值集合
-    element *hufftree=new element[2*8-1];    // 动态创建数组
-    HuffmanTree(hufftree, x, 8);
-    print(hufftree,15);
-    system("pause");
     return 0;
 }
