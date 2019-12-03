b935b4874eeba53128674a5128f6ccd57e22720a
liuwei-cope <liuweiyuanzhibing@163.com>
2019-12-03 20:53:02 +0800

    A1812 liuwei

--- liuwei/shiyan1/3.c
+++ liuwei/shiyan1/3.c
@@ -1,29 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include"3.h"
 
-int *g(int n)
-{
-	int *ptr_arr=(int *)malloc(sizeof(int)*n);
-	for (int i=0;i<n;i++)
+
+int *g(int n){
+
+	int *ptr_arr = (int *)malloc(sizeof(int)*n);
+	for (int i = 0; i < n; i++)
 	{
-		ptr_arr[i]=i+1;
+		ptr_arr[i] = i+1;
 	}
-	printf("初始化的值和地址为：、\n");
-	for(int i=0;i<n;i++)
+	printf("初始化的值和地址为: \n");
+	for (int i = 0; i < n; i++)
 	{
-		printf("ptr_arr[%d]=%d\t",i,ptr_arr[i]);
-		printf("&ptr_arr[%d]=%p\n",i,&ptr_arr[i]);
+		printf("ptr_arr[%d] = %d\t",i,ptr_arr[i] );
+		printf("&ptr_arr[%d] = %p\n",i,&ptr_arr[i] );
 	}
 	return ptr_arr;
-}
-
-
-
-int main(int argc,char **argv)
-{
-	int n;
-	int ptr_arr;
-	scanf("%d",&n);
-	ptr_arr=g(n);
-	return 0;
 }
