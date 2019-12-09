f12d6f3cc889772aa8ceb8154db8d349c7e4e84c
yuguanghui <ygh09110@163.com>
2019-12-09 22:14:28 +0800

    yuguanghui

---
+++ ygh/sh1/3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>                               
+int main(int argv,char **argc)
+{int m=0,n=0,p=m+n,s;	
+printf("初始定义的值:");	
+printf("%d,%p\n",p,&p);
+printf("please input your first data:\n");
+scanf("%d",&m);
+printf("please input your add data:\n");
+scanf("%d",&n);
+int *ptr_arr=(int*)malloc(p);
+printf("打印初始的指针地址\n");
+printf("%p\n",ptr_arr);
+p=m+n;
+printf("输入增量后内存增加\n");
+printf("%d\n",p);
+printf("打印输入增量后的指针地址\n");
+printf("%p\n",ptr_arr);
+s= *ptr_arr;
+free(ptr_arr);
+return 0;
+}
