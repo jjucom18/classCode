f2cc6ad1f6a043ff279737060b59dc04770b66ea
huangwuhuibbd <huangwuhuibbd@163.com>
2019-12-21 13:39:39 +0800

    hwh

--- huangwuhui/text3.c
+++ huangwuhui/text3.c
@@ -1,22 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(int argc,char **argv)
-{
-int *ptr_arr;
-int N;
-printf("input N:\n");
-scanf("%d",&N);
-if(N<=0) N=100;
-ptr_arr=(int*)malloc(sizeof(int)*N);
-if(!ptr_arr){
-	printf("alloc ptr_arr error !\n");
-	exit(0);
-	
-}else{
-	printf("alloc ptr_arr success !\n")；
-}
 
-	
-	
-	
-}
+
+void main() 
+{
+	int *ptr_arr = (int *) malloc(sizeof(int) ) ;
+	int a;
+	int i;
+	printf("请输入一个数字\n") ;
+	scanf("%d",&a) ;
+	for(i=0;i<a;i++) 
+{
+ ptr_arr[i]  = i+a;
+ printf("%d\t%p\n",ptr_arr[i] ,&ptr_arr[i] ) ;} } 
--- huangwuhui/text4.c
+++ huangwuhui/text4.c
@@ -1,2 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
+void vary(){
+	int var1,*p,maxsize,i=0;
+	printf("输入动态分配内存大小maxsize的值");
+	scanf("%d\n",&maxsize);
+	p=(int*)malloc(maxsize*sizeof(int));
+	printf("用户输入x的值\n");
+	scanf("%d\n",&var1);
+	while(i<maxsize)
+	{
+		p[i]=var1++;
+		printf("%d\t",p[i]);
+		i++;
+	}
+}
+void element(){
+	int var1,k,*ptr_cvar1;
+	printf("请输入常量cvar1的值和var1值\n");
+	const int cvar1=k;
+	scanf("%d\n%d\n",&k,&var1);
+	var1=var1;
+	const int *ptr_cvar=&cvar1;
+	ptr_cvar=&var1;
+	for()
+  var1++;  
+	printf("%d\n",var1);
+
+}
+
+void vary1(){
+	int var1,var,*cptr_var2,*cptr_var1;
+	printf("输入常量var1");
+	scanf("%d\n",&var1);
+	cptr_var1=&var1;
+	printf("输入常量var2");
+	scanf("%d\n",&var);
+	const int var2=var;
+	*cptr_var2=var2;
+	int *const p=&var1;
+	for(p;p<p+var2;cptr_var1++)
+	{
+		var1++;
+		printf("%d\t",var1);
+	}
+}
