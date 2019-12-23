60e24a3c7947a34c6a6a9c351f90ed6b4387bcf9
tongyanhe <tsy19981226@163.com>
2019-12-22 17:48:20 -0800

    20191223

---
+++ tongyanhe1125/qimo2.c/textc2.c
@@ -0,0 +1,19 @@
+#include"qimo2.h"
+
+int main(int argc, char **argv) {
+    printf("**************************************************************\n");
+    printf("                                                              \n");
+    printf("              Welcome to the little Internet bar !            \n");
+    printf("                                                              \n");
+    printf("**************************************************************\n");
+    while(1){
+    printf("                  0:exit distance                           \n");
+    printf("                  1:Enter Internet                            \n");
+    printf("                  2:Leave Internet                            \n");
+    printf("                  3:Log out                                   \n");
+    printf("                  Please choose :                             \n");
+    printf("**************************************************************\n");
+    Enter();
+    exit(0);
+    }
+}
