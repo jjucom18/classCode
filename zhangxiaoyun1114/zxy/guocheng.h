d814e58fae91b709310452df226175fa7567435f
yunyunyunshu <57093732+yunyunyunshu@users.noreply.github.com>
2019-12-23 12:36:42 +0800

    Create guocheng.h

---
+++ zhangxiaoyun1114/zxy/guocheng.h
@@ -0,0 +1,8 @@
+int Min(int a[], int n);    //返回长度为n的数组a[]的一个最小下标，从1开始
+int ShortestQueue();        //获取最短队列编号
+void OpenForDay();            //初始化操作
+void CustomerArriver();        //顾客到达事件
+void CustomerDeparture();    //顾客离开事件
+void Bank_Simulation();        //银行排队模拟
+void PrintEventList();        //输出事件列表
+void PrintQueue();            //打印当前队列
