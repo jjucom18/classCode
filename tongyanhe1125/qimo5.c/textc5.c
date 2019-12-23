4aac487c98a047f5fc81ac2adfc2e05e1c501d3a
tongyanhe <tsy19981226@163.com>
2019-12-23 09:57:56 -0800

    20191224

---
+++ tongyanhe1125/qimo5.c/textc5.c
@@ -0,0 +1,20 @@
+#include"qimo5.h"
+
+int main(int argc, char **argv) {
+    printf("**************************************************************\n");
+    printf("                                                              \n");
+    printf("              Welcome to the little Internet bar !            \n");
+    printf("                                                              \n");
+    printf("**************************************************************\n");
+    while(1){
+    printf("                  0:exit distance                             \n");
+    printf("                  1:Enter Internet                            \n");
+    printf("                  2:Leave Internet                            \n");
+    printf("                  3:Log out                                   \n");
+    printf("                  4:Map                                       \n");
+    printf("                  Please choose :                             \n");
+    printf("**************************************************************\n");
+    Enter();
+    exit(0);
+    }
+}
